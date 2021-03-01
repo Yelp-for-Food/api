3e74665535f045d2249ea1667959a2e2404bd5ab
Amman Sood <392075+asood92@users.noreply.github.com>
2021-03-01 14:14:02 -0500

    Exposed API routes
    Co-authored-by: DeJay <contact@dejaydev.com>

--- felp/models/meal.py
+++ felp/models/meal.py
@@ -1,6 +1,8 @@
-from . import BaseModel, PeeweeGetterDict
+from . import BaseModel
+from felp.sql import PeeweeGetterDict
 from peewee import TextField
 from pydantic import BaseModel as PydanticBaseModel
+
 
 @BaseModel.register
 class Meal(BaseModel):
@@ -9,18 +11,43 @@ class Meal(BaseModel):
     area = TextField()
     thumb = TextField()
 
-    class _MealBase(PydanticBaseModel): # The thing, internally.
+    @staticmethod
+    def get_meal(meal_id):
+        # Meal.get(Meal.name == "name")
+        # Meal.get_by_id(id)
+        return Meal.get_by_id(meal_id)
+
+    @staticmethod
+    def get_all_meals():
+        return Meal.select().iterator()
+
+    @staticmethod
+    def get_meal_by_name(name: str):
+        return Meal.select().where(Meal.name.contains(name))
+
+    @staticmethod
+    def change_meal(meal_id: int, **data):
+        return Meal.get_by_id(meal_id).update(data)
+
+    class _MealBase(PydanticBaseModel):  # The thing, internally.
         name: str
         category: str
         area: str
         thumb: str
-        
-    class MealCreate(_MealBase): # The thing, but what it takes to make it
-        pass
-    
-    class MealSchema(_MealBase): # The thing, but what is returned
-        id: int
-        
+
         class Config:
             orm_mode = True
-            getter_dict = PeeweeGetterDict # Does some magic to turn lists into.. nothing.
+
+        # getter_dict = (
+        #    PeeweeGetterDict  # Does some magic to turn lists into.. nothing.
+        # )
+
+    class MealCreate(_MealBase):  # The thing, but what it takes to make it
+        pass
+
+    class MealSchema(_MealBase):  # The thing, but what is returned
+        name: str
+        category: str
+        area: str
+        thumb: str
+        id: int
--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 # Hi :)
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from felp.sql import db, db_state_default
 from felp.models import MODELS
-import felp.schemas as schemas
+from felp.models.meal import Meal
 
 db.connect()
 db.create_tables(MODELS)
 db.close()
 
 app = FastAPI()
+
 
 async def reset_db_state():
     db._state._state.set(db_state_default.copy())
@@ -23,10 +24,25 @@ def get_db(db_state=Depends(reset_db_state)):
         if not db.is_closed():
             db.close()
 
+
 @app.get("/")
 async def root():
     return "a"
 
-@app.get("/meals", response_model=schemas.Meal, dependencies=[Depends(get_db)])
+
+@app.get("/meals", response_model=Meal._MealBase, dependencies=[Depends(get_db)])
 async def meals():
-    return users
+    res = Meal.get()
+    return res
+
+
+@app.get(
+    "/meals/{meal_id}", response_model=Meal.MealSchema, dependencies=[Depends(get_db)]
+)
+async def meal_id(meal_id: int):
+    meal = Meal.get_meal(meal_id)
+
+    if meal is None:
+        raise HTTPException(status_code=404, detail="Meal not found")
+    else:
+        return meal
